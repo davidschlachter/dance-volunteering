5e071f7234e12c373c184af17b5032926e9501fc
David Schlachter <david+gh@schlachter.ca>
2016-06-27 10:29:27 -0400

    Add config file

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var routes = require('./routes/index');
 
 var app = express();
 
+// Get options from config file
+var config = require('./config');
+
 // Use Jade for templating
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
