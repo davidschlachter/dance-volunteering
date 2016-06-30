bd9f2224b0e54e44f28775624f0243878f1e4bfc
David Schlachter <david+gh@schlachter.ca>
2016-06-30 18:18:57 -0400

    Shrink cancellation button

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ app.use(cookieParser(config.opt.sessionsecret));
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
+  outputStyle: 'compressed',
+  indentedSyntax: false,
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
