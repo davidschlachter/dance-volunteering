6e50f580d72df52aa96aedfbb0c06e93de56e83a
David Schlachter <david+gh@schlachter.ca>
2016-07-01 12:13:50 -0400

    Fix path to User model

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var User = require('./models/userModel');
+var User = require('../models/userModel');
 
 // Get options from config file
 var config = require('../config');
