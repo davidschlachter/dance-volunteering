7295be167236a5d1ab060168130c966bbae189fd
David Schlachter <david+gh@schlachter.ca>
2016-07-05 08:20:36 -0400

    Fix indentation of userModel

--- models/userModel.js
+++ models/userModel.js
@@ -4,30 +4,30 @@ var mongoose = require('mongoose');
 
 // Define the todo schema
 var userSchema = new mongoose.Schema({
-	// UserIDs from OAuth
-	facebookID: String,
-	googleID: String,
-	liveID: String,
-	// Biographical
-	userName: String,
+  // UserIDs from OAuth
+  facebookID: String,
+  googleID: String,
+  liveID: String,
+  // Biographical
+  userName: String,
   firstName: String,
   lastName: String,
   lastNameInitial: String,
   profilePicture: String,
-	email: String,
-	emailConfirmed: { type: Boolean, default: false },
-	isNewUser: { type: Boolean, default: true },
-	isAdmin: { type: Boolean, default: false },
-	// Email preferences
+  email: String,
+  emailConfirmed: { type: Boolean, default: false },
+  isNewUser: { type: Boolean, default: true },
+  isAdmin: { type: Boolean, default: false },
+  // Email preferences
   sendNewShift: { type: Boolean, default: true },
   sendChangedShift: { type: Boolean, default: true },
   sendDeletedShift: { type: Boolean, default: true },
-	sendReminder: { type: Boolean, default: true },
-	sendThanks: { type: Boolean, default: true },
-	sendVolunteeringCall: { type: Boolean, default: true },
-	// Admin email preferences
-	sendSchedule: { type: Boolean, default: true },
-	sendDetails: { type: Boolean, default: false }
+  sendReminder: { type: Boolean, default: true },
+  sendThanks: { type: Boolean, default: true },
+  sendVolunteeringCall: { type: Boolean, default: true },
+  // Admin email preferences
+  sendSchedule: { type: Boolean, default: true },
+  sendDetails: { type: Boolean, default: false }
 });
 
 // Export the Mongoose model
