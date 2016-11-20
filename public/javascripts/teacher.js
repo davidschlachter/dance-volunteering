dc879056e23e699bd4cc86e8b72b45d608b8eed5
David Schlachter <david+gh@schlachter.ca>
2016-11-19 21:23:54 -0500

    Add placeholder Teaching tab

--- public/javascripts/teacher.js
+++ public/javascripts/teacher.js
@@ -1,1 +1,21 @@
 // Javascript for teacher functions
+
+$(document).ready(function () {
+  // Event listeners for the pill buttons
+  $('#teachingPill').on('click', switchToDoorShifts);
+  $('#doorShiftsPill').on('click', switchToTeaching);
+});
+
+function switchToDoorShifts() {
+  $("#teachingDiv").show();
+  $("#teachingPill").addClass('active');
+  $("#doorShiftsDiv").hide();
+  $("#doorShiftsPill").removeClass('active');
+}
+
+function switchToTeaching() {
+  $("#doorShiftsDiv").show();
+  $("#doorShiftsPill").addClass('active');
+  $("#teachingDiv").hide();
+  $("#teachingPill").removeClass('active');
+}
