a8561093c95e55d741eb0636e4df17ee1ef0e462
David Schlachter <david+gh@schlachter.ca>
2019-04-03 17:02:08 -0400

    Remove Javascript from cant-login

--- public/javascripts/cant-login.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  // Add an event listener for the Privacy Policy
-  $("a#showPrivacyPolicy").click(function (e) {
-    e.preventDefault();
-    $('#privacyPolicy').modal('show');
-    return false;
-  });
-  
-});
